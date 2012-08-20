9b6c63417bd7438151e46009ab07c02099c42208
Ken Stowell <ken@cdbaby.com>
2012-08-20 06:42:07 -0700

    Zend scaffolding and Layout files.
    Signed-off-by: Ken Stowell <ken@cdbaby.com>

---
+++ application/Bootstrap.php
@@ -0,0 +1,86 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+	/**
+	 *
+	 * @var unknown_type
+	 */
+
+	/**
+	 *
+	 */
+	protected function _initSession()
+	{
+		return Zend_Session::start();
+	}
+
+	/**
+	 *
+	 */
+	// protected function _initAutoload()
+	// {
+	// 	$this->_logger->info('Bootstrap ' . __METHOD__);
+
+	// 	// Add autoloader empty namespace
+	// 	$autoLoader = Zend_Loader_Autoloader::getInstance();
+	// 	$autoLoader->registerNamespace('KTS_');
+	// 	$resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+	// 		'basePath' => APPLICATION_PATH,
+	// 		'namespace' => '',
+	// 		'resourceTypes' => array(
+	// 			'form' => array(
+	// 				'path' => 'forms/',
+	// 				'namespace' => 'Form_'
+	// 			),
+	// 			'model' => array(
+	// 				'path' => 'models/',
+	// 				'namespace' => 'Model_'
+	// 			),
+	// 			'language' => array(
+	// 				'path' => 'language/',
+	// 				'namespace' => 'Language_'
+	// 			)
+	// 		)
+	// 	));
+	// 	// Return it so that it can be stored by the bootstrap
+	// 	return $autoLoader;
+	// }
+
+	/**
+	 *
+	 */
+	protected function _initLocale()
+	{
+
+		$locale = new Zend_Locale('en_US');
+		Zend_Registry::set('Zend_Locale', $locale);
+	}
+
+	/**
+	 * Sets various DOM info.
+	 */
+	protected function _initDoctype()
+	{
+
+		// Init the view
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('HTML5');
+
+		// Set title and separator
+		$view->headTitle('Threadgill Memorial')
+			->setSeparator(' | ');
+
+		// Styles
+		$view->headLink()->appendStylesheet('/styles/application/threadgill-memorial.css');
+
+		// Scripts
+		$view->headScript()->appendFile('/scripts/libs/jquery/jquery-1.7.1.js')
+			->headScript()->appendFile('/scripts/libs/modernizr/modernizr.js')
+			->headScript()->appendFile('/scripts/libs/1140/1140.js')
+			->headScript()->appendFile('/scripts/application/threadgill-memorial.js');
+	}
+}
+
---
+++ public/scripts/application/threadgill-memorial.js
@@ -0,0 +1,104 @@
+/* Javascript document
+ *
+ * @Author:	Ken Stowell
+ * @Date:		
+ *
+ */
+
+/**********************************************************************************************************************************************************
+ * GLOBAL VARS/FUNCTIONS																																																																	*
+ *********************************************************************************************************************************************************/
+
+/**********************************************************************************************************************************************************
+ *	Threadgill_Memorial
+ **********************************************************************************************************************************************************
+ *
+ * @desc: Main scripting resource for the Threadgill Memorial application.
+ *
+ *
+ *
+ *
+ */
+(function (global) {
+	/**
+	 * -------------------------------
+	 * Threadgill_Memorial CONSTRUCTOR
+	 * -------------------------------
+	 */
+	var Threadgill_Memorial = function () {
+		var self = this;
+
+		//instantiate object methods
+		this.init();
+	};
+
+	/**
+	 * ----------------------------------
+	 * Threadgill_Memorial OBJECT METHODS
+	 * ----------------------------------
+	 */
+	Threadgill_Memorial.prototype = {
+		/**
+		 * ----
+		 * INIT
+		 * ----
+		 */
+		init:function () {
+			var self = this;
+
+			// Load dynamic page elements
+			this.build_page();
+
+			// Bind DOM events
+			this.bind_events();
+		},
+		/**
+		 * -----
+		 * UTILS
+		 * -----
+		 * @desc: public object to modularize reptitive code/utilities
+		 */
+		_utils: {
+		
+		},
+		/**
+		 * ----------
+		 * BUILD PAGE
+		 * ----------
+		 * @desc: general DOM manipulation if necessary
+		 */
+		build_page:function () {
+			var self = this;
+
+			/**
+			 * Document Ready
+			 */
+			$(document).ready(function () {
+
+			});
+
+			/**
+			 * Window Load
+			 */
+			$(window).load(function () {
+
+			});
+		},
+		/**
+		 * -----------
+		 * BIND EVENTS
+		 * -----------
+		 * @desc: Object to house event bindings.
+		 */
+		bind_events: function() {
+			var self = this;
+
+		}
+	}
+	// Instantiate the local object and push it to the window object
+	window.TM = new Threadgill_Memorial();
+})(window);
+
+
+/************************************************************* END ***************************************************************************************/ 
+
