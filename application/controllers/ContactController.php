e8eef99643f5b0f554ca0e16100b53bc6bd37055
Ken Stowell <ktstowell@kenstowell.net>
2012-12-15 10:08:01 -0800

    Updated he submit action and thank-you submit action

--- application/controllers/ContactController.php
+++ application/controllers/ContactController.php
@@ -6,4 +6,44 @@ class ContactController extends Zend_Controller_Action
 	{
 
 	}
+
+	public function submitAction()
+	{
+
+		if(isset($_POST) && !empty($_POST)) {
+
+			$subject = 'Contact submission from www.threadgillmemorial.com';
+
+			// Change to deborah for production
+			$to = 'stowell.kt@gmail.com';
+
+			$body ='<h1>Contact for submission from threadgillmemorial.com</h1>';
+			$body .="\n";
+			$body .="<h2>Contact Name:</h2>";
+			$body .="\n";
+			$body .= $_POST['first-name'] . ' ' . $_POST['last-name'];
+			$body .="\n";
+			$body .="<h2>Contact Email:</h2>";
+			$body .="\n";
+			$body .=$_POST['email'];
+			$body .="\n";
+			$body .="<h2>Contact Phone:</h2>";
+			$body .="\n";
+			$body .=$_POST['phone'];
+			$body .="\n";
+			$body .="<h2>Contact Message:</h2>";
+			$body .="\n";
+			$body .=$_POST['inquiry'];
+
+			if($mail = new TMS_Mail_Mail($subject, $to, $body, NULL, NULL, FALSE)) {
+				$r = Zend_Controller_Action_HelperBroker::getStaticHelper('redirector');
+    		$r->gotoUrl('/contact/thank-you')->redirectAndExit();
+			}
+		}
+	}
+
+	public function thankYouAction()
+	{
+
+	}
 }
