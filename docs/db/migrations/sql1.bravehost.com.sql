b1943e7692440068dca9b4202cdc9762cf86e6da
Ken Stowell <ken@cdbaby.com>
2013-03-22 11:02:54 -0700

    Including db migrations

---
+++ docs/db/migrations/sql1.bravehost.com.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.3.1
+-- http://www.phpmyadmin.net
+--
+-- Host: sql1.bravehost.com
+-- Generation Time: Mar 19, 2013 at 04:47 PM
+-- Server version: 5.0.92
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `threadgill-dev_2344443`
+--
+CREATE DATABASE `threadgill-dev_2344443` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `threadgill-dev_2344443`;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
