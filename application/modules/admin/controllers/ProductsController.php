40e88f4793c68189a538cc14994c5f9072cddab5
Ken Stowell <ktstowell@kenstowell.net>
2013-03-22 22:54:21 -0700

    Updated add/edit methods to check for files before sending them to model method.

--- application/modules/admin/controllers/ProductsController.php
+++ application/modules/admin/controllers/ProductsController.php
@@ -24,14 +24,8 @@ class Admin_ProductsController extends Zend_Controller_Action
     }
 
     $product_model = new Admin_Model_Product();
-
-    $currProducts = $product_model->getProducts();
+		$currProducts = $product_model->getProducts();
     $currProductsArray = $currProducts->toArray();
-    // foreach ($currProductsArray as $rowArray) {
-    // 	foreach ($rowArray as $column => $value) {
-    		
-    // 	}
-    // }
     $this->view->products = $currProductsArray;
 	}
 
@@ -43,34 +37,107 @@ class Admin_ProductsController extends Zend_Controller_Action
 
 		$res = array();
 
+		$file = NULL;
+		$file_name = NULL;
+
 		foreach ($_POST as $key => $value) {
 			$res[$key] = $value;
 		}
 
-		$baseDir = APPLICATION_PATH . '/../public/images/' . $res['product_type'];
+		if(isset($_FILES) && array_key_exists('product_photo', $_FILES)) {
 
-    $file = $baseDir .'/' . $_FILES['product_photo']['name'];
+			$baseDir = APPLICATION_PATH . '/../public/images/' . $res['product_type'];
 
-    if(file_exists($file) == FALSE) {
-    	move_uploaded_file($_FILES['product_photo']['tmp_name'], $file);
-    }
+			$file = $baseDir .'/' . $_FILES['product_photo']['name'];
+			$rel_file_path = '/images/'. $res['product_type'] . '/' . $_FILES['product_photo']['name'];
+			$file_name = $_FILES['product_photo']['name'];
 
-    if(array_key_exists('product_visibility', $res)) {
-			$res['product_visibility'] = TRUE;
-		} else {
-			$res['product_visibility'] = FALSE;	
+	    if(file_exists($file) == FALSE) {
+	    	move_uploaded_file($_FILES['product_photo']['tmp_name'], $file);
+	    }
 		}
-			
+
   	$add = $product_model->addProduct(
 			$res['product_name'], 
 			$res['product_type'], 
 			$res['product_price'],
 			$res['product_desc'],
-			$file,
-			$_FILES['product_photo']['name'],
+			$rel_file_path,
+			$file_name,
 			$res['product_visibility']
 		);
 
-		return json_encode($add);
+		return json_encode($res);
+	}
+
+	public function editAction()
+	{
+
+		$this->_helper->layout->disableLayout();
+
+		$product_model = new Admin_Model_Product();
+    	
+  	$res = array();
+
+		$file = NULL;
+		$file_name = NULL;
+
+		foreach ($_POST as $key => $value) {
+			$res[$key] = $value;
+		}
+
+		if(isset($_FILES) && array_key_exists('product_photo', $_FILES)) {
+
+			$baseDir = APPLICATION_PATH . '/../public/images/' . $res['product_type'];
+
+			$file = $baseDir .'/' . $_FILES['product_photo']['name'];
+			$rel_file_path = '/images/'. $res['product_type'] . '/' . $_FILES['product_photo']['name'];
+			$file_name = $_FILES['product_photo']['name'];
+
+	    if(file_exists($file) == FALSE) {
+	    	move_uploaded_file($_FILES['product_photo']['tmp_name'], $file);
+	    }
+		}
+
+  	$edit = $product_model->editProduct(
+  		$res['product_id'], 
+			$res['product_name'], 
+			$res['product_type'], 
+			$res['product_price'],
+			$res['product_desc'],
+			$rel_file_path,
+			$file_name,
+			$res['product_visibility']
+		);
+
+		return json_encode($edit);
+	}
+
+	public function refreshAction() {
+		$this->_helper->layout->disableLayout();
+
+		$product_model = new Admin_Model_Product();
+		$currProducts = $product_model->getProducts();
+    $currProductsArray = $currProducts->toArray();
+    $res = $this->view->partialLoop('partials/_products.phtml', $currProductsArray);
+
+    echo json_encode($res);
+	}
+
+	public function deleteAction()
+	{
+		$this->_helper->layout->disableLayout();
+		
+		$product_model = new Admin_Model_Product();
+
+		$res = array();
+
+		foreach ($_POST as $key => $value) {
+			$res[$key] = $value;
+		}
+
+		$delete = $product_model->deleteProduct($res['id']);
+
+		return json_encode($delete);
 	}
 }
