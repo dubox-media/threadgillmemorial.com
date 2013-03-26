92e8633a4aa0174480987788f848b5cfa1657aa5
Ken Stowell <ken@cdbaby.com>
2013-03-26 00:38:54 -0700

    Prevents duplicate tags form being added to list.

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -12,7 +12,20 @@ class ProductsController extends Zend_Controller_Action
 		$product_model = new Admin_Model_Product();
 		$currProducts = $product_model->getProducts();
     $currProductsArray = $currProducts->toArray();
+    $tags = array();
+    foreach ($currProductsArray as $key => $value) {
+    	if($currProductsArray[$key]['tags'] != NULL && $currProductsArray[$key]['is_visible'] == 'true') {
+    		$unser = unserialize($currProductsArray[$key]['tags']);
+    		$str = preg_split("/,\s*/", $unser);
+    		foreach($str as $idx) {
+    			if(!in_array(Array('tag' => $idx), $tags)) {
+    				array_push($tags, Array('tag' => $idx));
+    			}
+    		}
+    	}
+    }
     $this->view->products = $currProductsArray;
+    $this->view->tags = $tags;
 	}
 
 	public function casketsAction()
